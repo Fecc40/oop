976a0fa5be72148a4a8d5a355a1329be3389149a
Fecc40 <hf@list.ru>
2016-06-29 21:20:05 +0300

    переименована функция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -14,18 +14,18 @@ namespace snake
             int y1 = 3;
             char sym1 = '*';
 
-            Drow(x1, y1, sym1);
+            Draw(x1, y1, sym1);
             
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Drow(x2, y2, sym2);
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
         }
 
-        static void Drow(int x, int y, char sym)
+        static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
