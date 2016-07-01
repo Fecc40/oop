b6cc218fe1b2ffb7ec4a189c0951c2d4490efcc0
Fecc40 <hf@list.ru>
2016-07-01 22:13:11 +0300

    созданы классы Horizontal+Vertical Line

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,35 +16,12 @@ namespace snake
              Point p2 = new Point(4, 5, '#');
              p2.Draw();
 
-             Point p3 = new Point(5, 2, '%');
-             p3.Draw();
+            Horizontalline line = new Horizontalline(5, 10, 8, '+');
+            line.Drow();
 
-             Point p4 = new Point(11, 7, '&');
-             p4.Draw();
-
-            List<char> numList = new List<char>();
-            numList.Add('*');
-            numList.Add('#');
-            numList.Add('%');
-            numList.Add('&');
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-           foreach(char i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            
+            Verticalline vLine = new Verticalline(10, 17, 15, 'x');
+            vLine.Drow();
+                        
             Console.ReadLine();
         }
     }
