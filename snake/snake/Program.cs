9b1417138a845968f6390c6016dc720c80a8d72d
Fecc40 <hf@list.ru>
2016-07-02 18:48:12 +0300

    создание класса Snake

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -22,11 +22,11 @@ namespace snake
             Verticalline rightLine = new Verticalline(0, 24, 78, '+');
             rightLine.Drow();
 
-            //Отрисовка точки
+            //Отрисовка змейки
             Point p = new Point(4, 5, '*');
-            p.Draw();
-
-                                  
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+                                              
             Console.ReadLine();
         }
     }
